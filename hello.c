4d697396740bd989715d82c06dd10bdf68f50fb9
Bikraman Karmakar <bikramankarmakar@Bikramans-MacBook-Air.local>
2026-03-16 15:50:22 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int 
+
+void insert(char key[], char value[], char array[][100]) {
+
+    printf("%s \n", value);
+
+    
+    
+}
+
+
+
+
+int main() {
+
+    char array[100][100]; 
+
+    insert("215", "Bikraman Karmakar", array);
+    return 0;
+}
+
+
+
